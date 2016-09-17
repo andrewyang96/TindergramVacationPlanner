58aa60ed0334d5058221e57bd4b4d4510edb225e
Andrew Yang <andrewyang96@gmail.com>
2016-09-17 12:08:17 -0400

    Add logout routes and user detection

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,22 @@ var router = express.Router();
 var passport = require('passport');
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    user: req.user,
+  });
 });
 
 router.get('/login', passport.authenticate('facebook'));
 
 router.get('/login/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
   function (req, res) {
-    res.send(req.user);
+    res.redirect('/');
   });
+
+router.get('/logout', function (req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
