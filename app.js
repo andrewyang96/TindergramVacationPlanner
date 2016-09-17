15fc00c79c146f969fbe194bfce1dc733f85592e
Andrew Yang <andrewyang96@gmail.com>
2016-09-17 12:03:14 -0400

    Basic auth flow

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Passport
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +25,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('express-session')({ secret: 'travel the world' }));
+
+// Fetch Facebook credentials
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgresql://root@localhost:26257?sslmode=disable';
+pg.connect(connectionString, function (err, client, done) {
+  if (err) {
+    done();
+    throw err;
+  }
+  client.query("SELECT client_id, client_secret FROM tgvp.credentials WHERE platform = 'fb'",
+    function (err, result) {
+      var client_id = result.rows[0].client_id;
+      var client_secret = result.rows[0].client_secret;
+
+      // Setup passport
+      passport.use(new Strategy({
+        clientID: client_id,
+        clientSecret: client_secret,
+        callbackURL: '/login/callback',
+      }, function (access_token, refresh_token, profile, cb) {
+        console.log(profile);
+        return cb(null, profile);
+      }));
+
+      passport.serializeUser(function(user, cb) {
+        cb(null, user);
+      });
+
+      passport.deserializeUser(function(obj, cb) {
+        cb(null, obj);
+      });
+
+      done();
+    });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+console.log('Passport configured');
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', passport.authenticate('facebook'));
+
+router.get('/login/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
+  function (req, res) {
+    res.send(req.user);
+  });
 
 module.exports = router;
